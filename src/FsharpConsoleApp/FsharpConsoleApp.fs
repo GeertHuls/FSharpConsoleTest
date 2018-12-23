2e23f63a6f6843b49034320da2aa6278c24900eb
Geert Huls <geert.huls82@gmail.com>
2018-12-23 18:05:48 +0100

    Use class lib in console app

--- src/FsharpConsoleApp/FsharpConsoleApp.fs
+++ src/FsharpConsoleApp/FsharpConsoleApp.fs
@@ -1,6 +1,14 @@
 module FsharpConsoleApp
 
+open ClassLibTest
+
 [<EntryPoint>]
 let main argv =
-    printfn "%A" argv
+
+    let testRecord = {
+        prop1 = "abc";
+        prop2 = "def";
+    }
+
+    printfn "%A %s %s" argv testRecord.prop1 testRecord.prop2
     0 // return an integer exit code
